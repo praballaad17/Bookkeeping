42698f6507cfd8a4b81c0889291c9f3f6517797c
vikas6577 <vs509411@gmail.com>
2022-05-10 15:50:48 +0530

    left sidebar of dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import * as ROUTES from './constants/routes';
+// import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './Components/Dashboard';
+// import * as ROUTES from './constants/routes';
 
-import MainLoader from './loader/mainLoader';
+// import MainLoader from './loader/mainLoader';
 
-const Login = lazy(() => import('./pages/Login'));
-const SignUp = lazy(() => import('./pages/Signup'));
+// const Login = lazy(() => import('./pages/Login'));
+// const SignUp = lazy(() => import('./pages/Signup'));
 // const Dashboard = lazy(() => import('./pages/dashboard'));
 // const NotFound = lazy(() => import('./pages/not-found'));
 
@@ -13,20 +14,24 @@ export default function App() {
   // const { user, jwt } = useAuthListener()
 
   return (
-
-    <Router>
-      <Suspense fallback={<MainLoader />}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={() => <Login />} />
-          <Route path={ROUTES.SIGN_UP} component={() => <SignUp />} />
+    <>
+     <Router>
+      {/* <Suspense fallback={<MainLoader />}> */}
+      
+        {/* <Routes> */}
+          {/* <Route path={ROUTES.LOGIN} component={() => <Login />} /> */}
+          {/* <Route path={ROUTES.SIGN_UP} component={() => <SignUp />} /> */}
           {/* <Route path={ROUTES.DASHBOARD} component={() => <Dashboard />} /> */}
           {/* <ProtectedRoute user={user} path={ROUTES.DASHBOARD}  >
                   <Dashboard />
                 </ProtectedRoute> */}
-          <Route component={NotFound} />
-        </Switch>
-      </Suspense>
-    </Router>
+           
+          {/* <Route exact path='/Dashboard' component={<Dashboard/>} /> */}
+          <Dashboard/>
+        {/* </Routes> */}
+      {/* </Suspense> */}
+     </Router>
+    </>
 
   );
 }
