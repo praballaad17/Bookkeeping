ea8efd70abf5d68c7cf31da26c1a191485284399
vikas6577 <vs509411@gmail.com>
2022-05-26 14:47:31 +0530

    items table updated

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,94 +1,111 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import * as ROUTES from "../../constants/routes";
-import { useUser } from '../../Context/userContext';
-import { getItemsByUserId } from '../../services/ItemServices';
-import ItemIndividual from './ItemIndividual';
-import ItemList from './ItemList';
+import { useUser } from "../../Context/userContext";
+import { getItemsByUserId } from "../../services/ItemServices";
+import ItemIndividual from "./ItemIndividual";
+import ItemList from "./ItemList";
 // import "../css/item.css";
 
 export default function Item() {
-    const { user } = useUser();
-    const [items, setItems] = useState([])
+  const { user } = useUser();
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        const getItems = async () => {
-            try {
-                const itemArray = await getItemsByUserId(user?.id, 10, 1)
-                console.log(itemArray);
-                setItems(itemArray)
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        getItems()
-    }, [])
+  useEffect(() => {
+    const getItems = async () => {
+      try {
+        const itemArray = await getItemsByUserId(user?.id, 10, 1);
+        console.log(itemArray);
+        setItems(itemArray);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getItems();
+  }, []);
 
-    return (
-        <>
-            {items && items !== null ?
-                <div className='items'>
-                    <div className='item__heading'>Items</div>
-                    <div className='items__box'>
-                        <div className='items__box--left'>
-                            <input type="search" id="searchitem" name="searchitem"/>
-                                <div className="productdetails">
-                                <span className='fullname'>FULLNAME</span>
-                                <span className='quantity'>QUANTITY</span>
-                                
-                                    
-                                     <ItemList items={items} />
-                                 </div>
-            
-
-                        </div>
-                        <div className='items__box--right'>
-                            <div className="items__box--right__upper">
-                            <span>BUNDLES</span>
-                            <button className=" items__box--right__upper--button ">Add Conversion</button> 
-                            </div>
-                            <div className="items__box--right__lower">
-                            <span className='fullname'>UNITS</span>
-                            <input type="search"  className=" items__box--right__lower--search " /> 
-                            </div>
-                            {/* <ItemIndividual /> */}
-                        </div>
-                    </div>
-                    {/* <div className='item__list'>
-                        <table className='importtable__table'>
-                            <thead>
-                                <tr>
-                                    <th>Item Code</th>
-                                    <th>Item Name</th>
-                                    <th>HNS</th>
-                                    <th>Sale Price</th>
-                                    <th>Purchase Price</th>
-                                    <th>Discount Type</th>
-                                    <th>Sale Discount</th>
-                                    <th>Opening Stock</th>
-                                    <th>Minimum_stock_quantity</th>
-                                    <th>Item_Location</th>
-                                    <th>Tax_Rate</th>
-                                    <th>Inclusive_Of_Tax</th>
-                                </tr>
-                            </thead>
-                            {
-                                items.map((item) => (
-                                    <tbody>
-                                        <ItemIndividual item={item} />
-                                    </tbody>
-                                ))
-                            }
-                        </table>
-                    </div> */}
-                </div> :
-                <div className="products">
-                    <img src="https://static.vecteezy.com/system/resources/previews/005/073/073/original/no-item-in-the-shopping-cart-add-product-click-to-shop-now-concept-illustration-flat-design-eps10-modern-graphic-element-for-landing-page-empty-state-ui-infographic-icon-vector.jpg" className='productimage' alt="" />
-                    <p className='producttext'>Add Products/Items you sell or purchase to manage your full Stock Inventory.</p>
-                    <Link to={ROUTES.ADDITEM} className='add-item-button'>Add Your First Product</Link>
+  return (
+    <>
+      {items && items !== null ? (
+        <div className="items">
+          <div className="item__heading">Items</div>
+          <div className="items__box">
+            <div className="items__box--left">
+              <input type="search" id="searchitem" name="searchitem" />
+              <div className="productdetails">
+                {/* <span className="fullname">FULLNAME</span>
+                <span className="quantity">QUANTITY</span> */}
+                <table className="importtable__table">
+                    <thead className="theadbold">
+                        <tr>
+                            <th>FULLNAME</th>
+                            <th>QUANTITY</th>
+                        </tr>
+                    </thead>
+                </table>
+                {/* <ItemList items={items} /> */}
+                
+              </div>
+            </div>
+            <div className="items__box--right">
+              <div className="items__box--right__upper">
+                <span>BUNDLES</span>
+                <button className=" items__box--right__upper--button ">
+                  Add Conversion
+                </button>
+              </div>
+              <div className="items__box--right__lower">
+                <span className="fullname">UNITS</span>
+                <input
+                  type="search"
+                  className=" items__box--right__lower--search "
+                />
+                <div className="item__list">
+                  <table className="importtable__table">
+                    <thead>
+                      <tr>
+                        <th>Item Code</th>
+                        <th>Item Name</th>
+                        <th>HNS</th>
+                        <th>Sale Price</th>
+                        <th>Purchase Price</th>
+                        <th>Discount Type</th>
+                        <th>Sale Discount</th>
+                        <th>Opening Stock</th>
+                        <th>Minimum_stock_quantity</th>
+                        <th>Item_Location</th>
+                        <th>Tax_Rate</th>
+                        <th>Inclusive_Of_Tax</th>
+                      </tr>
+                    </thead>
+                    {items.map((item) => (
+                      <tbody>
+                        <ItemIndividual item={item} />
+                      </tbody>
+                    ))}
+                  </table>
                 </div>
-            }
-
-        </>
-    )
+              </div>
+              {/* <ItemIndividual /> */}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="products">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/005/073/073/original/no-item-in-the-shopping-cart-add-product-click-to-shop-now-concept-illustration-flat-design-eps10-modern-graphic-element-for-landing-page-empty-state-ui-infographic-icon-vector.jpg"
+            className="productimage"
+            alt=""
+          />
+          <p className="producttext">
+            Add Products/Items you sell or purchase to manage your full Stock
+            Inventory.
+          </p>
+          <Link to={ROUTES.ADDITEM} className="add-item-button">
+            Add Your First Product
+          </Link>
+        </div>
+      )}
+    </>
+  );
 }
