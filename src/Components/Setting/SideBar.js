760e0fe5345233c507b87895db4c26be5c5621bc
praballaad17 <praballaad17@gmail.com>
2022-06-26 19:55:34 +0530

    backbutton in settings

--- src/Components/Setting/SideBar.js
+++ src/Components/Setting/SideBar.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function SideBar() {
+    const navigate = useNavigate()
     return (
         <div className='settingsidebar'>
             <button disabled style={{ "fontSize": "20px", "backgroundColor": "black", "color": "white", "padding": "10px 10px 10px 15px" }}>SETTINGS<i className="fa-solid fa-magnifying-glass" style={{ "marginLeft": "80px" }}></i></button>
+            <button className='btn btn--primary' onClick={() => navigate('/')}> <i className="fa-solid fa-arrow-left editbox--back"></i>Back To Dashboard</button>
             <hr />
             <a href="/setting/general ">GENERAL</a>
             <a href="#transaction ">TRANSACTION</a>
