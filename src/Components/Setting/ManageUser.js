1abf7425f60145cee6c15950e064523b23ca9788
vikas6577 <vs509411@gmail.com>
2022-07-08 23:40:07 +0530

    manage user created in setting

--- src/Components/Setting/ManageUser.js
+++ src/Components/Setting/ManageUser.js
@@ -1,8 +1,34 @@
 import React from 'react'
-
+import imageone from './leftmanageuser.png';
+import imagetwo from './rightmanageuser.png';
 const ManageUser = () => {
   return (
-    <div>ManageUser</div>
+    <>
+     <div className="purinvoice__head" style={{ height: "65px" }}>
+        <div className="leftheadpurchase">
+          <span style={{ fontSize: "22px", paddingTop: "10px" }}>
+            Manage Users
+          </span>
+        </div>
+        <div className="rightheadpurchase">
+          <button className="possalesbutton">Use Roles<i style={{"paddingLeft":"7px"}} className="fa-solid fa-circle-question"></i></button>
+          <button className="possalesbutton">Add New User</button>
+        </div>
+      </div>
+      <div className="manageuserbody">
+        <div>
+        <img style={{"width":"550px","padding":"15px"}} src={imageone} alt="logo" />
+        </div>
+        <div>
+        <img style={{"width":"550px","padding":"15px"}} src={imagetwo} alt="logo" />
+        </div>
+      </div>
+      <div>
+      <button className="possalesbutton" style={{"marginLeft":"575px","backgroundColor":"blue","color":"white"}}>+ Add New Staff</button>
+      <p style={{"textAlign":"center","fontSize":"15px","fontWeight":"400"}}>To learn more about other features different Roles are allowed to use click</p>
+
+      </div>
+    </>
   )
 }
 
