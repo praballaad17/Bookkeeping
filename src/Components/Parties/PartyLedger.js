9b7ee7330403d67e255579ccd14cbe1f4a8f5da8
Saksham <saksham.kathuria0211@gmail.com>
2022-07-13 11:44:20 +0530

    Added Party Leger and Profile

--- src/Components/Parties/PartyLedger.js
+++ src/Components/Parties/PartyLedger.js
@@ -1,7 +1,83 @@
-import React from 'react'
+import React from "react";
 
 export default function PartyLedger() {
     return (
-        <div>PartyLedger</div>
-    )
+        <div className="party-ledger">
+            <div className="upper-container">
+                <div className="input-buttons">
+                    <select className="date-filter">
+                        <option value="NA">All time</option>
+                    </select>
+                </div>
+                <div className="print-buttons">
+                    <div className="print-section">Download</div>
+                </div>
+                <div className="print-buttons">
+                    <div className="print-section">Print</div>
+                </div>
+                <div className="print-buttons">
+                    <div className="print-section">Send SMS</div>
+                </div>
+            </div>
+
+            <div className="ledger-list">
+                <div class="ledger-details">
+                    <div class="company-header">
+                        <div class="column-1">
+                            <div class="company-name"> NA </div>
+                            <div class="mobile"> Phone no: &nbsp; &nbsp; 7206150211 </div>
+                        </div>
+                        <div class="ledger-title"> Party Ledger </div>
+                    </div>
+                    <div class="party-details">
+                        <div class="column-1">
+                            <div class="to">To,</div>
+                            <div class="party-name text-ellipsis">
+                                {" "}
+                                Cash Sale <br />{" "}
+                            </div>
+                            <div class="mobile">7206150211</div>
+                        </div>
+                        <div class="column-2">
+                            <div class="date"> 2022-07-13 - 2022-07-13 </div>
+                            <div class="text"> Total Payable </div>
+                            <div class="value"> 0 </div>
+                        </div>
+                    </div>
+                    <div class="ledger-txns">
+                        <table class="ledger-table">
+                            <thead>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>Voucher</th>
+                                    <th>Sr No</th>
+                                    <th>Credit</th>
+                                    <th>Debit</th>
+                                    <th>Balance</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td> Opening Balance </td>
+                                    <td></td>
+                                    <td> - </td>
+                                    <td> _ </td>
+                                    <td>0</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td> Closing Balance </td>
+                                    <td></td>
+                                    <td> - </td>
+                                    <td> _ </td>
+                                    <td>0</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/Components/Parties/PartyProfile.js
+++ src/Components/Parties/PartyProfile.js
@@ -1,7 +1,145 @@
-import React from 'react'
-
+import React from "react";
+import "../../css/style.css"
 export default function PartyProfile() {
     return (
-        <div>PartyProfile</div>
-    )
+        <div className="party-profile">
+            <div className="general-info">
+                <div className="container">
+                    <div className="flex-columns">
+                        <div className="column-title">
+                            Party Name
+                        </div>
+                        <div className="column-value">
+
+                            Cash Sale
+                        </div>
+                    </div>
+                    <div className="flex-columns">
+                        <div className="column-title">
+                            Mobile Number
+                        </div>
+                        <div className="column-value">
+
+                            1525252
+                        </div>
+                    </div>
+                    <div className="flex-columns">
+                        <div className="column-title">
+                            Email
+                        </div>
+                        <div className="column-value">
+
+                            -
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="flex-columns">
+                        <div className="column-title">
+                            Party Category
+                        </div>
+                        <div className="column-value">
+                            <div className="category">
+
+                                -
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex-columns">
+                        <div className="column-title">
+                            Opening Balance
+                        </div>
+                        <div className="column-value">
+                            <div className="balance">
+                                <div className="sign"></div>
+                                <div className="value">
+
+                                    ₹ 0
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+
+                className="general-info "
+            >
+                <div className="container">
+                    <div className="flex-columns w-50">
+                        <div className="column-title">
+                            GSTIN
+                        </div>
+                        <div className="column-value">
+
+                            _
+                        </div>
+                    </div>
+                    <div className="flex-columns">
+                        <div className="column-title">
+                            Place of Supply
+                        </div>
+                        <div className="column-value">
+
+                            -
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="flex-columns w-50">
+                        <div className="column-title">
+                            Billing Address
+                        </div>
+                        <div className="column-value">
+
+                            -
+                        </div>
+                    </div>
+                    <div className="flex-columns w-50">
+                        <div
+
+                            className="d-flex justify-content-between"
+                        >
+                            <div className="column-title">
+                                Shipping Address
+                            </div>
+                        </div>
+                        <div className="column-value">
+
+                            -
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+
+                className="general-info "
+            >
+                <div className="container">
+                    <div className="flex-columns w-50">
+                        <div className="column-title">
+                            Credit Period
+                        </div>
+                        <div className="column-value">
+
+                            0 days
+                        </div>
+                    </div>
+                    <div className="flex-columns">
+                        <div className="column-title">
+                            Credit Limit
+                        </div>
+                        <div className="column-value">
+
+                            ₹ 0
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+
+                className="general-info  flex-wrap flex-directtion-row"
+            ></div>
+        </div>
+    );
 }
